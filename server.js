5220ea120be729b51dd79d3d146b924aebc4fca6
Leon <leonbaraza@gmail.com>
2020-06-18 12:08:19 +0300

    Home page view

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 
 const app = express();
 
 // Set view engine
 app.set('view engine', 'ejs');
 
-// Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// use routes
+app.use('/articles', articleRouter);
 
+app.get('/', (req, res) => {
+    articles = [
+        {
+            title: "Test Article",
+            createdAt: new Date(),
+            description: "Test Description comes here"
+        },
+        {
+            title: "Test Article 1",
+            createdAt: new Date(),
+            description: "Test Description comes here 1"
+        }
+    ];
+    res.render('index', { text: 'Hello', articles });
+});
 
 // Define the port and listen
 const PORT = process.env.PORT || 3000;
